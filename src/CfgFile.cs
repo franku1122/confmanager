b59365b6d7f03d2be11ad59e8a7a367f581fc282
franku1122 <163011306+franku1122@users.noreply.github.com>
2025-02-21 16:41:59 +0100

    nowy plik:       ConfManager.csproj 	nowy plik:       confmanager.sln 	nowy plik:       obj/ConfManager.csproj.nuget.dgspec.json 	nowy plik:       obj/ConfManager.csproj.nuget.g.props 	nowy plik:       obj/ConfManager.csproj.nuget.g.targets 	nowy plik:       obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	nowy plik:       obj/Debug/net8.0/ConfManager.AssemblyInfo.cs 	nowy plik:       obj/Debug/net8.0/ConfManager.AssemblyInfoInputs.cache 	nowy plik:       obj/Debug/net8.0/ConfManager.GeneratedMSBuildEditorConfig.editorconfig 	nowy plik:       obj/Debug/net8.0/ConfManager.GlobalUsings.g.cs 	nowy plik:       obj/Debug/net8.0/ConfManager.assets.cache 	nowy plik:       obj/project.assets.json 	nowy plik:       obj/project.nuget.cache 	nowy plik:       src/CfgFile.cs

---
+++ src/CfgFile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+namespace ConfManager;
+
+/// <summary>
+/// A class storing the data of a loaded .cfg file
+/// </summary>
+public sealed class CfgFile
+{
+    
+}
