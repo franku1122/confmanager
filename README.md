9163589ed440cbdfe04b8909b6d1a4fcc3c112d0
franku1122 <163011306+franku1122@users.noreply.github.com>
2025-02-21 17:33:39 +0100

    zmieniono:       README.md 	zmieniono:       obj/Debug/net8.0/ConfManager.AssemblyInfo.cs 	zmieniono:       obj/Debug/net8.0/ConfManager.AssemblyInfoInputs.cache

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ An optimized .cfg file reader / writer, used in my projects ( and games ).
 For more details on each feature, check the docs.
 
 - [ ] Annotations ( special flags at the top of the file )
-- [ ] Customizable line endings
-- [ ] Customizable bools
+- [ ] Customizable bools ( instead of true / false, you could use 1 / 0, on / off, etc. )
 - [ ] Customizable comment characters
 - [ ] Customizable key/value separator
 - [x] Customizable logger ( eg. use `GD.Print` instead of `Console.WriteLine` )
--- obj/Debug/net8.0/ConfManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConfManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59365b6d7f03d2be11ad59e8a7a367f581fc282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28c70e2f7b4709cbd7c5f7ab4f70a69848c4710a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
