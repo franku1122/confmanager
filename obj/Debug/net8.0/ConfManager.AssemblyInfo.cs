1e7c3715c2adb6b2e7f311469479f0ab5e952c58
franku1122 <163011306+franku1122@users.noreply.github.com>
2025-02-22 11:40:27 +0100

    zmieniono:       .gitignore 	zmieniono:       ConfManager.csproj 	zmieniono:       obj/Debug/net8.0/ConfManager.AssemblyInfo.cs 	zmieniono:       obj/Debug/net8.0/ConfManager.AssemblyInfoInputs.cache

--- obj/Debug/net8.0/ConfManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConfManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef17cacb86a73dd66d28a9593fc3139a9021e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20dcd9e9294dd28a66c97b4d27f9b7c9a5d5590e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
